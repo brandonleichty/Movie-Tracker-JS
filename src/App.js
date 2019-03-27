780bda77362c73465e0d6ad1b9f710779e35a34e
Brandon Leichty <leichty@gmail.com>
2019-03-27 17:19:37 -0500

    Convert App component to functional component and add initial state and hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+const App = () => {
+  // Since this is a relatively small app, I'll be storing state required throughout the
+  // entire app in the "App" component.
+
+  // Site wide state includes:
+  // 1. "user" - if a user is logged in, this will hold the user object returned from Firebase. By defaul it's set to null.
+
+  // 2. "welcomePage" (maybe rename this variable? 🧐) - if it's the user's first time visiting the page, and they're not currently
+  //    logged in, users will be shown a welcome screen that invites them to add the first movie to their collection.
+  //    welcomePage is set to "true" by default;
+
+  // 3. "movieGrid" is the array that will store all the movies being displayed on the screen. It's an empty array by default.
+  //    Once data is fetched from TMDB it'll be filled with movies.
+
+  // 4. "userMovies" is the array that will hold each movie that a user has seen (onces they've clicked on). If no user is logged in,
+  //    the users movies will only be stored in the local "App" state. Once a user is logged in, the state will sync in "real time" with Firebase/Firestore.
+
+  // 5. I will be using emojis in my code comments. 🙌 I'm sorry if that offends anyone. 😜
+
+  const [user, setUser] = useState(null);
+  const [welcomePage, setWelcomePage] = useState(true);
+  const [movieGrid, setMovieGrid] = useState([]);
+  const [userMovies, setUserMovies] = useState([]);
+
+  return (
+    <div className="outer-container">
+      <Header />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
