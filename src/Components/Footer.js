35f3279a28ca02124c307dd2498f7fb20c11dbe1
Brandon Leichty <leichty@gmail.com>
2019-03-27 17:16:38 -0500

    Add intial Footer component

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <span>Created in Minnesota by Brandon Leichty</span>
+    </footer>
+  );
+};
+
+export default Footer;
