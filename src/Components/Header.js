cfd2abaab22c94c935559d1dfafd7089d77ba3d8
Brandon Leichty <leichty@gmail.com>
2019-03-30 15:18:19 -0500

    Add IntroPage component with conditional logic

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,23 @@
 import React from "react";
+import NavBar from "./NavBar";
+import SearchBar from "./Search";
 
 import logo from "../Images/theater.svg";
 
-const Header = () => {
+const Header = props => {
+  const { setNavBarLocation, setQuery, introPage } = props;
   return (
     <header>
       <img src={logo} className="site-hero-logo" alt="movie-tracker-js-logo" />
       <div className="movie-tracker-title">
         <h1>MOVIE TRACKER</h1>
       </div>
+      {introPage ? null : (
+        <>
+          <NavBar setNavBarLocation={setNavBarLocation} />
+          <SearchBar setQuery={setQuery} />
+        </>
+      )}
     </header>
   );
 };
