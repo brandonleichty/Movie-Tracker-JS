e51d3dc41691d9c8e72ccb9782b2567dc8a93487
Brandon Leichty <leichty@gmail.com>
2019-03-31 22:08:27 -0500

    Add addOrRemoveMovies hook on button click. And change button view based upon state of userMovies

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,15 +1,26 @@
 import React from "react";
 
+import addIcon from "../Images/add.svg";
+import checkedIcon from "../Images/tick.svg";
 const POSTER_PATH = "http://image.tmdb.org/t/p/w500";
 
 const Movie = props => {
-  const { movie } = props;
+  const { movie, userMovies, addOrRemoveUserMovies } = props;
 
   return (
     <div className="movie-container">
       <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
 
-      <button className="add-button" />
+      <button
+        className="add-button"
+        onClick={() => addOrRemoveUserMovies(movie)}
+      >
+        {userMovies.some(myMovie => myMovie["id"] === movie.id) ? (
+          <img src={checkedIcon} alt="" />
+        ) : (
+          <img src={addIcon} alt="" />
+        )}
+      </button>
     </div>
   );
 };
