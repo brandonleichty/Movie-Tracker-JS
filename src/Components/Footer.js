27721d6c9e200e6666db6bbc18b976804c6dc064
Brandon Leichty <leichty@gmail.com>
2019-03-31 22:04:01 -0500

    Add comments

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+// At this point the footer is pretty darn basic. I plan on adding a few other simple things such as
+// a link to my twitter and an embedded link to brandonleichty.com (or my github since my site isn't up yet).
+
 const Footer = () => {
   return (
     <footer>
