c16d962baf1744390d4da1febe1b5e1ff632e609
Brandon Leichty <leichty@gmail.com>
2019-03-31 22:03:39 -0500

    Add movieType (or NavBarLocation), query, userMovies to props

--- src/Components/MovieGrid.js
+++ src/Components/MovieGrid.js
@@ -4,14 +4,24 @@ import Movie from "./Movie";
 import { useFetchMovies } from "../Hooks/fetchMoviesHook";
 
 const MovieGrid = props => {
-  const [movieGrid, setMovieGrid] = useFetchMovies(props.movieType);
+  const [movieGrid, setMovieGrid] = useFetchMovies(
+    props.movieType,
+    props.query,
+    props.userMovies
+  );
 
+  const { userMovies, addOrRemoveUserMovies } = props;
   return (
     <div className="movie-grid-container">
       {movieGrid
         .filter(movie => movie.adult === false)
         .map(movie => (
-          <Movie key={movie.id} movie={movie} />
+          <Movie
+            key={movie.id}
+            movie={movie}
+            userMovies={userMovies}
+            addOrRemoveUserMovies={addOrRemoveUserMovies}
+          />
         ))}
     </div>
   );
