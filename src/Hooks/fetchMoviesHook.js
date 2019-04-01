bed8d0f049c2e64da41228a724a638d675a255ab
Brandon Leichty <leichty@gmail.com>
2019-03-31 22:21:22 -0500

    Add logic that hit's a different API endpoint based upon the NavBarLocation state value

--- src/Hooks/fetchMoviesHook.js
+++ src/Hooks/fetchMoviesHook.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 // Takes in a movie prop (that's an object) and iterates over it. While filtering out adult movies.
 
-function useFetchMovies(typeOfMovies) {
+function useFetchMovies(typeOfMovies, query = null, userMovies = null) {
   const [movieData, setMovieData] = useState([]);
 
   // This function returns a string value which is the TMDB endpoint for either "popular" or "upcoming" movies.
@@ -20,20 +20,39 @@ function useFetchMovies(typeOfMovies) {
 
   const movieEndpointURL = `https://api.themoviedb.org/3/movie/${typeOfMovies}?api_key=d951026be8c262501cf4a37f22f82184&language=en-US&page=1&region=US`;
 
+  const searchURL = `https://api.themoviedb.org/3/search/movie?api_key=d951026be8c262501cf4a37f22f82184&language=en-US&query=${query}&page=1&include_adult=false`;
+
   const fetchMovieData = async () => {
-    try {
-      const res = await fetch(movieEndpointURL);
-      const movies = await res.json();
-      setMovieData(movies.results);
-      console.log(movies.results);
-    } catch (err) {
-      console.log(err);
+    // Need to clean up this code and figure out if there's a better way to handle the logic. Here's what it's currently doing:
+    // 1. If the typeOfMovies argument is equal to "search", set the dataToFetch to the TMDB query API.
+    // 2. Else if the above is not true set it to the TMDB movie search endpoint.
+    // 3. If the query argument does NOT equals an empty string, try and fetch any data from the API. Also fetch the data if the
+    //    typeOfMovie is equal to "popular" or "upcoming"
+
+    const dataToFetch =
+      typeOfMovies === "search" ? searchURL : movieEndpointURL;
+    if (
+      query !== "" ||
+      typeOfMovies === "popular" ||
+      typeOfMovies === "upcoming"
+    ) {
+      try {
+        const res = await fetch(dataToFetch);
+        const movies = await res.json();
+
+        //This returns an array of movie objects. Need to look into if this is the bestion. Maybe use just objects without the array?
+        setMovieData(movies.results);
+      } catch (err) {
+        console.log(err);
+      }
+    } else if (typeOfMovies === "myMovies") {
+      setMovieData(userMovies);
     }
   };
 
   useEffect(() => {
     fetchMovieData();
-  }, [typeOfMovies]);
+  }, [typeOfMovies, query, userMovies]);
 
   return [movieData, setMovieData];
 }
