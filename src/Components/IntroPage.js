14fa51876329346f7ff503372a5751d7d4576d08
Brandon Leichty <leichty@gmail.com>
2019-03-31 22:07:24 -0500

    Add comments

--- src/Components/IntroPage.js
+++ src/Components/IntroPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import addIcon from "../Images/add.svg";
 
+// If it's the users first visit to the site, or they've visted before but don't have any selected movies, this intro page
+// will be shown if "introPage" prop is set to false. Would like to revist the design of this in the future.
+
 const IntroPage = props => {
   const { setIntroPage } = props;
 
