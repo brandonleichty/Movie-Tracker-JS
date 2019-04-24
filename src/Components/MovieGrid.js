e72dbf762784d8ce5e2143b8e1dbe83c5f5764a4
Brandon Leichty <leichty@gmail.com>
2019-04-24 15:29:28 -0500

    Add functionallity that changes grid based upon nav bar location or numbe of selected movies

--- src/Components/MovieGrid.js
+++ src/Components/MovieGrid.js
@@ -1,30 +1,59 @@
 import React, { useState, useEffect } from "react";
 import Movie from "./Movie";
+import LoadMoreMovies from "./LoadMoreMovies";
+import EmptyMovies from "./EmptyMovies";
 
 import { useFetchMovies } from "../Hooks/fetchMoviesHook";
 
 const MovieGrid = props => {
   const [movieGrid, setMovieGrid] = useFetchMovies(
     props.movieType,
+    props.setQuery,
     props.query,
     props.userMovies
   );
 
-  const { userMovies, setUserMovies, loginStatus, user } = props;
+  const {
+    userMovies,
+    setUserMovies,
+    loginStatus,
+    user,
+    movieType,
+    hideNavBar
+  } = props;
+
+  // Make sure the nav bar is't hidden
+
+  hideNavBar(false);
+
   return (
-    <div className="movie-grid-container">
-      {movieGrid
-        .filter(movie => movie.adult === false)
-        .map(movie => (
-          <Movie
-            key={movie.id}
-            user={user}
-            movie={movie}
-            loginStatus={loginStatus}
-            userMovies={userMovies}
-            setUserMovies={setUserMovies}
-          />
-        ))}
+    <div>
+      {movieType === "myMovies" && userMovies.length === 0 ? (
+        <EmptyMovies loginStatus={loginStatus} />
+      ) : (
+        <div className="movie-grid-container">
+          {movieGrid
+            .filter(movie => movie.adult === false)
+            .map(movie => (
+              <Movie
+                key={movie.id}
+                user={user}
+                movie={movie}
+                loginStatus={loginStatus}
+                userMovies={userMovies}
+                setUserMovies={setUserMovies}
+                navBarLocation={movieType}
+              />
+            ))}
+        </div>
+      )}
+      {movieType === "popular" || movieType === "upcoming" ? (
+        <LoadMoreMovies
+          movieGrid={movieGrid}
+          setMovieGrid={setMovieGrid}
+          movieType={movieType}
+        />
+      ) : null}
     </div>
   );
 };
