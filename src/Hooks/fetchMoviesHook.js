b34dfa2f4f2d6023b9d6cd4e4385020833715bc3
Brandon Leichty <leichty@gmail.com>
2019-04-24 15:59:46 -0500

    Add search ability

--- src/Hooks/fetchMoviesHook.js
+++ src/Hooks/fetchMoviesHook.js
@@ -2,8 +2,14 @@ import React, { useState, useEffect } from "react";
 
 // Takes in a movie prop (that's an object) and iterates over it. While filtering out adult movies.
 
-function useFetchMovies(typeOfMovies, query = null, userMovies = null) {
+function useFetchMovies(
+  typeOfMovies,
+  setQuery,
+  query = null,
+  userMovies = null
+) {
   const [movieData, setMovieData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   // This function returns a string value which is the TMDB endpoint for either "popular" or "upcoming" movies.
   // A few things to look into:
@@ -18,7 +24,7 @@ function useFetchMovies(typeOfMovies, query = null, userMovies = null) {
   //   }
   // };
 
-  const movieEndpointURL = `https://api.themoviedb.org/3/movie/${typeOfMovies}?api_key=d951026be8c262501cf4a37f22f82184&language=en-US&media_type=movie&page=1&primary_release_year=2019&sort_by=popularity.desc&vote_count`;
+  const movieEndpointURL = `https://api.themoviedb.org/3/movie/${typeOfMovies}?api_key=d951026be8c262501cf4a37f22f82184&language=en-US&media_type=movie&page=1&primary_release_year=2019&sort_by=popularity.desc&vote_count&region=US`;
 
   const searchURL = `https://api.themoviedb.org/3/search/movie?api_key=d951026be8c262501cf4a37f22f82184&language=en-US&query=${query}&page=1&include_adult=false`;
 
@@ -31,28 +37,54 @@ function useFetchMovies(typeOfMovies, query = null, userMovies = null) {
 
     const dataToFetch =
       typeOfMovies === "search" ? searchURL : movieEndpointURL;
-    if (
-      query !== "" ||
-      typeOfMovies === "popular" ||
-      typeOfMovies === "upcoming"
-    ) {
+    if (typeOfMovies === "popular" || typeOfMovies === "upcoming") {
+      setQuery("");
       try {
+        setLoading(true);
         const res = await fetch(dataToFetch);
         const movies = await res.json();
 
+        setLoading(false);
+
         //This returns an array of movie objects. Need to look into if this is the bestion. Maybe use just objects without the array?
-        setMovieData(movies.results);
+        if (loading === false) {
+          setMovieData(movies.results);
+        }
       } catch (err) {
         console.log(err);
       }
-    } else if (typeOfMovies === "myMovies") {
+    }
+
+    if (typeOfMovies === "myMovies") {
+      setQuery("");
       setMovieData(userMovies);
+    }
+
+    if (typeOfMovies === "search") {
+      if (typeof query === "undefined" || query === "" || query === null) {
+        setMovieData([]);
+      } else {
+        try {
+          setLoading(true);
+          const res = await fetch(dataToFetch);
+          const movies = await res.json();
+
+          setLoading(false);
+
+          //This returns an array of movie objects. Need to look into if this is the bestion. Maybe use just objects without the array?
+          if (loading === false) {
+            setMovieData(movies.results);
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      }
     }
   };
 
   useEffect(() => {
     fetchMovieData();
-  }, [typeOfMovies, query, userMovies]);
+  }, [typeOfMovies, query]);
 
   return [movieData, setMovieData];
 }
