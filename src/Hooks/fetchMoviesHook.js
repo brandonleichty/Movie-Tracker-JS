f6be414042942963159883f67903cee8d25e425e
Brandon Leichty <leichty@gmail.com>
2019-05-30 18:54:38 -0500

    Change endpoint based upong NavBar location

--- src/Hooks/fetchMoviesHook.js
+++ src/Hooks/fetchMoviesHook.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
 
 // Takes in a movie prop (that's an object) and iterates over it. While filtering out adult movies.
 
@@ -24,10 +25,6 @@ function useFetchMovies(
   //   }
   // };
 
-  const movieEndpointURL = `https://api.themoviedb.org/3/movie/${typeOfMovies}?api_key=d951026be8c262501cf4a37f22f82184&language=en-US&media_type=movie&page=1&primary_release_year=2019&sort_by=popularity.desc&vote_count&region=US`;
-
-  const searchURL = `https://api.themoviedb.org/3/search/movie?api_key=d951026be8c262501cf4a37f22f82184&language=en-US&query=${query}&page=1&include_adult=false`;
-
   const fetchMovieData = async () => {
     // Need to clean up this code and figure out if there's a better way to handle the logic. Here's what it's currently doing:
     // 1. If the typeOfMovies argument is equal to "search", set the dataToFetch to the TMDB query API.
@@ -35,14 +32,32 @@ function useFetchMovies(
     // 3. If the query argument does NOT equals an empty string, try and fetch any data from the API. Also fetch the data if the
     //    typeOfMovie is equal to "popular" or "upcoming"
 
-    const dataToFetch =
-      typeOfMovies === "search" ? searchURL : movieEndpointURL;
+    const movieDatabaseEndpoint = navBarLocation => {
+      const date = moment();
+
+      if (navBarLocation === "upcoming") {
+        return `https://api.themoviedb.org/3/discover/movie?api_key=d951026be8c262501cf4a37f22f82184&page=1&language=en-US&primary_release_date.gte=${date.format(
+          "YYYY-MM-DD"
+        )}&sort_by=primary_release_date.asc&with_release_type=3&include_video=false&region=US`;
+      }
+
+      if (navBarLocation === "popular") {
+        return `https://api.themoviedb.org/3/movie/${typeOfMovies}?api_key=d951026be8c262501cf4a37f22f82184&language=en-US&media_type=movie&page=1&primary_release_year=2019&sort_by=popularity.desc&vote_count&region=US`;
+      }
+
+      if (navBarLocation === "search") {
+        return `https://api.themoviedb.org/3/search/movie?api_key=d951026be8c262501cf4a37f22f82184&language=en-US&query=${query}&page=1&include_adult=false`;
+      }
+    };
+
+    const dataToFetch = movieDatabaseEndpoint(typeOfMovies);
     if (typeOfMovies === "popular" || typeOfMovies === "upcoming") {
       setQuery("");
       try {
         setLoading(true);
         const res = await fetch(dataToFetch);
         const movies = await res.json();
+        console.log(movies);
 
         setLoading(false);
 
