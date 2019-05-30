b8a0a13bdd0f1e31c72cce8684e5208507b5454e
Brandon Leichty <leichty@gmail.com>
2019-05-30 18:55:49 -0500

    Change to calendar icon based upon nav bar location

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { useTransition, animated } from "react-spring";
+import moment from "moment";
 
 import addIcon from "../Images/add.svg";
+import calendarAddIcon from "../Images/calendar-add.svg";
+import calendarCheckIcon from "../Images/calendar-check-solid.svg";
 import checkedIcon from "../Images/tick.svg";
 const POSTER_PATH = "http://image.tmdb.org/t/p/w500";
 
 const Movie = props => {
-  // HOOKS
   const [show, set] = useState(false);
   const transitions = useTransition(show, null, {
     from: { opacity: 0 },
@@ -14,22 +16,51 @@ const Movie = props => {
     leave: { opacity: 0 }
   });
 
-  const { movie, userMovies, setUserMovies, loginStatus, user } = props;
+  const {
+    movie,
+    userMovies,
+    setUserMovies,
+    loginStatus,
+    user,
+    navBarLocation,
+    setWatchList,
+    watchList
+  } = props;
+
+  const releaseDate = moment(movie.release_date);
 
   return transitions.map(({ item, key, props }) => (
     <animated.div style={props} key={movie.id} className="movie-container">
       <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
 
-      <button
-        className="add-button"
-        onClick={() => setUserMovies(movie, user, loginStatus)}
-      >
-        {userMovies.some(myMovie => myMovie["id"] === movie.id) ? (
-          <img src={checkedIcon} alt="" />
-        ) : (
-          <img src={addIcon} alt="" />
-        )}
-      </button>
+      {navBarLocation === "popular" ? (
+        <button
+          className="add-button"
+          onClick={() => setUserMovies(movie, user, loginStatus)}
+        >
+          {userMovies.some(myMovie => myMovie["id"] === movie.id) ? (
+            <img src={checkedIcon} alt="" />
+          ) : (
+            <img src={addIcon} alt="" />
+          )}
+        </button>
+      ) : null}
+
+      {navBarLocation === "upcoming" ? (
+        <button
+          className="add-button"
+          onClick={() => setWatchList(movie, user, loginStatus)}
+        >
+          {watchList.some(
+            watchListMovie => watchListMovie["id"] === movie.id
+          ) ? (
+            <img src={checkedIcon} alt="" />
+          ) : (
+            <img src={calendarAddIcon} alt="" />
+          )}
+          <div className="movieButtonDate">{releaseDate.format("MMM Do")}</div>
+        </button>
+      ) : null}
     </animated.div>
   ));
 };
